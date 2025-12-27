f365c4d50fc25dbfdf09575ac460fd344e4464a2
M.Venelinova <148716527+Miroslava-Venelinova@users.noreply.github.com>
2025-12-27 12:40:50 +0200

    Commit file for data deletion via USP

---
+++ SQLQuery_Deletes.sql
@@ -0,0 +1,17 @@
+EXEC usp_DeleteAddress 4
+
+EXEC usp_DeleteGrade 2
+
+EXEC usp_DeleteMajor 8
+
+EXEC usp_DeleteMajorFee 7
+
+EXEC usp_DeleteStudent 'D9XH1M4CW0'
+
+EXEC usp_DeleteStudentSubjectPair 'KSTFO87YRP', 3
+
+EXEC usp_DeleteSubject 12
+
+EXEC usp_DeleteTeacher 4
+
+EXEC usp_DeleteTeacherSubjectPair 9, 11
